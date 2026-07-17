ebb32d6e13251a0184af033802e2e488c61d746d
Isaac Rocha <prog.isaac13@gmail.com>
2026-07-16 23:33:14 -0300

    Update conexao.php

--- database/conexao.php
+++ database/conexao.php
@@ -1,7 +1,7 @@
 <?php 
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=projeto_sos;port=3306", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=sos;port=3306", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
