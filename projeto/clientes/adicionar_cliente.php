86d2692ae8e4c180a411d71ed22a775b945985b5
Isaac Rocha <prog.isaac13@gmail.com>
2026-07-17 23:57:36 -0300

    Update adicionar_cliente.php

--- projeto/clientes/adicionar_cliente.php
+++ projeto/clientes/adicionar_cliente.php
@@ -6,7 +6,7 @@ $telefone = $_POST['telefone'] ?? null;
 $endereco = $_POST['endereco'] ?? null;
 $cep = $_POST['cep'] ?? null;
 
-$query = $pdo->query("INSERT INTO clientes (nome, telefone, enredeco, cep, equipamentos) VALUES ('$nome', '$telefone', '$endereco', '$cep' '0')");
+$query = $pdo->query("INSERT INTO clientes (nome, telefone, enredeco, equipamentos) VALUES ('$nome', '$telefone', '$endereco', '0')");
 if ($query) {
     $query = $pdo->query("SELECT id_cliente FROM clientes order by id_cliente desc;");
     $res = $query->fetchAll(PDO::FETCH_ASSOC);
